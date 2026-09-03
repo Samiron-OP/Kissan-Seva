9b3b5ab7e1a9800494d14e0d83191cd699907a5c
Samiron-OP <77061429+Samiron-OP@users.noreply.github.com>
2026-09-03 10:35:37 -0700

    Add files via upload

--- header.php
+++ header.php
@@ -1,17 +1,13 @@
-<?php echo "<!DOCTYPE html>
-<head>
-    <link rel='stylesheet' href='CSS//nav_style.css'>
-    <link rel = 'stylesheet' href='CSS//style.css'>
-    <link rel='preconnect' href='https://fonts.googleapis.com'>
-    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-    <link href='https://fonts.googleapis.com/css2?family=Caacupe+One&display=swap' rel='stylesheet'>
-</head>
-
-<body>
-        <nav class='navbar'>
-                <div class='logo'>
-                  <a class='caacupe-one-regular' href='#'>Kissan Seva</a>
-                </div>
-        </nav>
-"
+<?php echo "
+<nav class='navbar'>
+<div class='logo'>
+  <a class='caacupe-one-regular' href='index.php'>Kissan Seva</a>
+</div>
+<div class='profile'>
+  <a href='profile.php'>
+    <img src='Images/user.png' alt='Profile' class='profile-img'>
+  </a>
+</div>
+</nav>
+";
 ?>
